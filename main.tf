b5af5e8b3504b003f25c59c9c84fded6b5d4cd65
alopez-onica <104650249+alopez-onica@users.noreply.github.com>
2024-01-08 15:22:42 -0600

    load balancer

--- main.tf
+++ main.tf
@@ -29,20 +29,57 @@ module "blog_vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id= module.blog_vpc.public_subnets[0]
+  subnet_id              = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
+  }
+}
+
+module "blog-alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name            = "my-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+ listeners = [
+  {
+    port               =80
+    protocol           = "HTTP"
+    target_group_index = 0
+  }
+ ]
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "Example"
   }
 }
 
@@ -50,10 +87,10 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   
-  name = "blog"
-  vpc_id = module.blog_vpc.vpc_id
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  name                = "blog"
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
