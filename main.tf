1684ef334962089ffc82f2ff43191baa65634cea
alopez-onica <104650249+alopez-onica@users.noreply.github.com>
2024-01-08 15:46:06 -0600

    update alb module version

--- main.tf
+++ main.tf
@@ -46,33 +46,40 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog-alb" {
-  source = "terraform-aws-modules/alb/aws"
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 5.0"
 
-  name            = "blob-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
-  vpc_id             = module.blog_vpc.vpc_id
+
+  vpc_id             = module.blog_vpc/vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    ex-target = {
-      name_prefix = "blog-"
-      protocol    = "TCP"
-      port        = 80
-    }
-  }
-}
-
-   listeners = {
-    ex-tcp = {
-      port     = 80
-      protocol = "TCP"
-      forward = {
-        target_group_key = "ex-target"
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+            target_id = aws_instance.blog.id
+            port      = 80
+        }
       }
     }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
