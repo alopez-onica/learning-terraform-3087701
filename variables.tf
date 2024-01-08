a1c9ed817cf17620597389e06285197ce6f8c0ed
alopez-onica <104650249+alopez-onica@users.noreply.github.com>
2024-01-08 16:54:19 -0600

    update varilables file

--- variables.tf
+++ variables.tf
@@ -31,16 +31,6 @@ variable "environment" {
   }
 }
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
 variable "asg_min_size" {
   description = "Minimum number of instances in the ASG"
   default     = 1
